3048d5dde91847f4d0950ca39129f8460b118092
denis <furik1102@gmail.com>
2018-09-04 16:59:09 +0300

    main_start_3

--- app/js/common.js
+++ app/js/common.js
@@ -5,4 +5,34 @@ $(function () {
         selectSearch: true
     });
 
+    //counter
+    $('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+
+            $({ countNum: $this.text()}).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 3000,
+                    easing:'linear',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
+
+                });
+        });
+    });
+
+
 });
