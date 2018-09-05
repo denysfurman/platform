752405841f0ef3c69a5dc494e9a77060267953d8
denis <furik1102@gmail.com>
2018-09-05 14:03:13 +0300

    chart

--- app/js/common.js
+++ app/js/common.js
@@ -35,4 +35,80 @@ $(function () {
     });
 
 
+
+
+});
+$(function() {
+
+    Highcharts.theme = {
+        colors: ['#cac096', '#dbd4b8', '#ece8da', '#8eb03b', '#a2c038', '#b8d434'],
+        chart: {
+        }
+    };
+    // Apply the theme
+    Highcharts.setOptions(Highcharts.theme);
+
+
+
+});
+
+$(function() {
+    // Create the chart
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart_1',
+            type: 'pie'
+        },
+        title: {
+            text: 'Sold businesses'
+        },
+        yAxis: {
+            title: false
+        },
+        plotOptions: {
+            pie: {
+                shadow: false
+            }
+        },
+        tooltip: {
+            useHTML: true,
+            borderWidth:0,
+            borderRadius:0,
+            shadow: false,
+            padding: 10,
+
+            backgroundColor: "rgba(255,255,255,1)",
+
+            formatter: function() {
+                return  this.y + "<br/>" + this.point.name;
+            }
+        },
+        series: [{
+            name: 'Browsers',
+            data: [
+                ['sold businesses in\n' +
+                'North America', 32830],
+                ['sold businesses\n' +
+                'in Europe', 15780],
+                ['sold businesses\n' +
+                'in Asia', 8150],
+
+            ],
+            size: '60%',
+            innerSize: '60%',
+            borderWidth: 4,
+            showInLegend: false,
+
+            dataLabels: {
+                enabled: true,
+                useHTML: true,
+
+                format: '<b>{point.y}</b><br/><small>{point.name}</small>',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+
+        }]
+    });
 });
